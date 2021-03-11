91e925dff0daeaafe47b65744f122ec9a85fd87d
Aviral Agarwal <aviralagarwal25@gmail.com>
2021-03-11 18:26:05 +0530

    Remove nltk library

--- Naive-Bayes/preprocessing.py
+++ Naive-Bayes/preprocessing.py
@@ -1,14 +1,133 @@
-from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-import nltk
-
-def download_nltk_deps():
-    """
-    Downloads NLTK English language datasets. This function needs to be called only once in a lifetime.
-    """
-    nltk.download("punkt")
-    nltk.download("stopwords")
+DELIMITER = [" ", "\n", "\t", ",", "!", "."]
+STOPWORDS = [
+    "i",
+    "me",
+    "my",
+    "myself",
+    "we",
+    "our",
+    "ours",
+    "ourselves",
+    "you",
+    "your",
+    "yours",
+    "yourself",
+    "yourselves",
+    "he",
+    "him",
+    "his",
+    "himself",
+    "she",
+    "her",
+    "hers",
+    "herself",
+    "it",
+    "its",
+    "itself",
+    "they",
+    "them",
+    "their",
+    "theirs",
+    "themselves",
+    "what",
+    "which",
+    "who",
+    "whom",
+    "this",
+    "that",
+    "these",
+    "those",
+    "am",
+    "is",
+    "are",
+    "was",
+    "were",
+    "be",
+    "been",
+    "being",
+    "have",
+    "has",
+    "had",
+    "having",
+    "do",
+    "does",
+    "did",
+    "doing",
+    "a",
+    "an",
+    "the",
+    "and",
+    "but",
+    "if",
+    "or",
+    "because",
+    "as",
+    "until",
+    "while",
+    "of",
+    "at",
+    "by",
+    "for",
+    "with",
+    "about",
+    "against",
+    "between",
+    "into",
+    "through",
+    "during",
+    "before",
+    "after",
+    "above",
+    "below",
+    "to",
+    "from",
+    "up",
+    "down",
+    "in",
+    "out",
+    "on",
+    "off",
+    "over",
+    "under",
+    "again",
+    "further",
+    "then",
+    "once",
+    "here",
+    "there",
+    "when",
+    "where",
+    "why",
+    "how",
+    "all",
+    "any",
+    "both",
+    "each",
+    "few",
+    "more",
+    "most",
+    "other",
+    "some",
+    "such",
+    "no",
+    "nor",
+    "not",
+    "only",
+    "own",
+    "same",
+    "so",
+    "than",
+    "too",
+    "very",
+    "s",
+    "t",
+    "can",
+    "will",
+    "just",
+    "don",
+    "should",
+    "now",
+]
 
 
 def tokenize(text):
@@ -18,32 +137,27 @@ def tokenize(text):
     like comma, question mark, whitespace, etc.
     """
     assert type(text) == str
-    tokenized_words = word_tokenize(text.lower())
+    token = str()
+    tokenized_words = list()
+    for char in text:
+        if not char in DELIMITER:
+            token += char
+        else:
+            if token:
+                tokenized_words.append(token)
+            token = str()
+    if token:
+        tokenized_words.append(token)
+    return tokenized_words
 
-    # Remove tokens like comma, question mark and other punctuation.
-    filtered_words = list(filter(lambda word: len(word) > 1, tokenized_words))
-    return filtered_words
 
 def remove_stopwords(token_list):
     """
     Given a list of english words, removes stopwords from it ( such as he her was etc.).
     """
-    stop_words = set(stopwords.words("english"))
-    filtered_tokens = [w for w in token_list if not w in stop_words]
+    filtered_tokens = [w for w in token_list if not w in STOPWORDS]
     return filtered_tokens
 
-
-def stem(token_list):
-    """
-    Given an list of tokenized words, returns their stemmed forms, using NLTK
-    For example, `["playing"]` becoms `["play"]`.
-    """
-
-    assert type(token_list) == list
-    ps = PorterStemmer()
-    stemmed_words = list(map(lambda word: ps.stem(word), token_list))
-    result = " ".join(map(str, stemmed_words))
-    return result
 
 def process_string(query):
     """
@@ -54,6 +168,4 @@ def process_string(query):
     disposable = query.strip()
     disposable = tokenize(disposable)
     disposable = remove_stopwords(disposable)
-    disposable = stem(disposable)
-    return disposable.split(" ")
-    
+    return disposable
